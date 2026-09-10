2d0fc6df63a5afe7b656d8c1e6936f0835684da2
CuriosityLight <keegan.carter@ucas-edu.net>
2026-09-10 14:34:48 -0600

    push

---
+++ variables_practice.py
@@ -0,0 +1,14 @@
+# KC, variables practice
+
+name = input("what is your name? ") .capitalize()
+number1 = input("pick a number between 1 and 10. ") 
+number2 = input("pick a number between 100 and 1000. ")
+breakfast = input("what did you have for breakfast? ") .lower()
+color = input("whats your favorite color? ") .lower()
+school = input("what school do you go to? ") .capitalize()
+year = input("what year is it? ")
+eye = input("what is your eye color? ") .lower()
+age = input("what is your age? ")
+subject = input("whats your favorite subject in school? ") .lower()
+
+print(name, number1, number2, breakfast, color, school, year, eye, age, subject)
