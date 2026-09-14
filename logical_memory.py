7f8f179364ac6196ad723660be6f6e40968f27d6
CuriosityLight <keegan.carter@ucas-edu.net>
2026-09-14 14:02:09 -0600

    pull

---
+++ logical_memory.py
@@ -0,0 +1,9 @@
+# KC, logical and physical memory notes
+
+pi = 3.14159265
+
+school = "ucas"
+
+print(f"the variable pi has a value of {pi} and is saved at {id(pi)}")
+
+print(f"the variable school has a value of {school} and is saved at {id(school)}")
