9d07f3c1d782fbf6a2c821a08ca81913fbdc1977
CuriosityLight <keegan.carter@ucas-edu.net>
2026-09-17 14:33:59 -0600

    hi

--- your_budget.py
+++ your_budget.py
@@ -37,15 +37,18 @@ while True:
 
 
 
-rent_percent = (rent/income) * 100
-util_percent = (utilities/income) * 100
-groc_percent = (groceries/income) * 100
-transp_precent = (transportation/income) * 100
+rent_percent = int((rent/income) * 100)
+util_percent = int((utilities/income) * 100)
+groc_percent = int((groceries/income) * 100)
+transp_precent = int((transportation/income) * 100)
 
-print(f"your rent is $ {rent} and that is {rent_percent:.2f} % of your income")
-print(f"your utilities is $ {utilities} and that is {util_percent:.2f}% of your income")
-print(f"your groceries is $ {groceries} and that is {groc_percent:.2f}% of your income")
-print(f"your transportation is ${transportation} and that is, {transp_precent:.2f}% of your income")
+
+
+
+print(f"your rent is $ {rent:.2f} and that is {rent_percent} % of your income")
+print(f"your utilities is $ {utilities:.2f} and that is {util_percent}% of your income")
+print(f"your groceries is $ {groceries:.2f} and that is {groc_percent}% of your income")
+print(f"your transportation is ${transportation:.2f} and that is, {transp_precent}% of your income")
 
 total = income - (rent + utilities + groceries + transportation)
 print("you hvae $", total, "left to spend on anything each month. ")
