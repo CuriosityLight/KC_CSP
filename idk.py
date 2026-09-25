240d3ccbe6411f94abd0e18ea7ff71c4b80455b7
CuriosityLight <keegan.carter@ucas-edu.net>
2026-09-25 21:31:52 +0000

    a

--- idk.py
+++ idk.py
@@ -6,18 +6,21 @@ choice_player = ""
 choice = ""
 bet_amount = 0
 rand = SystemRandom()
-
-def funny(choice_player, choice, coins, bet_amount):
+fun_again = ""
+def funny(choice_player, choice, coins, bet_amount, fun_again):
     while True:
         while True:
             print("you have", coins, "coins")
             fun_again = input("bet again? ") .lower()
+            while True:
+                if fun_again == "yes":
+                    bet_amount = int(input("how much would you like to bet. "))
+                    if bet_amount <= coins:
+                        break
+                    else:
+                        print("you dont have enough to do that bet")
             if fun_again == "yes":
-                bet_amount = int(input("how much would you like to bet. "))
-                if bet_amount <= coins:
-                    break
-                else:
-                    
+                break
             elif fun_again == "no":
                 return
         choice_player = str(input("place a bet on black, red, green or a specific number. ")) .lower()
@@ -26,21 +29,19 @@ def funny(choice_player, choice, coins, bet_amount):
         if choice == "0 green":
             print("0 green")
             if choice_player == "0" or "green":
-                coins += bet_amount
+                coins += (bet_amount * 10)
                 print("you won, you now have", coins, "coins")
             else:
                 print("you lost")
-                coins /= 2
+                coins -= bet_amount
         elif choice == "1 black":
             if choice_player == "1":
-                coins *= 4
-                return
+                coins += bet_amount
             elif choice_player == "black":
-                coins *= 2
-                return
+                coins += bet_amount
             else:
-                coins /= 2
-funny(choice_player, choice, coins, bet_amount)
+                coins -= bet_amount
+funny(choice_player, choice, coins, bet_amount, fun_again)
 
 
 
